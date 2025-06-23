75197a6f47419c16416d44ca3d3a236fa57335a8
Girija Ray <91793906+Girijaray07@users.noreply.github.com>
2025-06-23 21:29:09 +0530

    Added Functionalities

--- CalculatorCLIApp.py
+++ CalculatorCLIApp.py
@@ -1,38 +1,51 @@
 class Calculator:
-    def __init__(self) -> None:
-        pass
     def sum(self):
-        total = 0; arr = []
-        count = int(input("Enter no. of Inputs: "))
-        for _ in range(count):
-            n = int(input("Enter: "))
-            arr.append(n)
-            total += n
-        print(f"The Sum of {arr}  is: {total}")
+        total, arr = 0, []
+        try:
+            count = int(input("Enter number of inputs: "))
+            for _ in range(count):
+                n = int(input("Enter: "))
+                arr.append(n)
+                total += n
+            return f"The Sum of {arr} is: {total}"
+        except Exception as e:
+            return f"Error: {e}"
+
     def sub(self):
-        n1 = int(input("Enter 1st Num: "))
-        n2 = int(input("Enter 2nd Num: "))
-        print(f"The Substract of {n1} and {n2} is: {n1-n2}")
+        try:
+            n1 = int(input("Enter 1st Num: "))
+            n2 = int(input("Enter 2nd Num: "))
+            return f"The Subtract of {n1} and {n2} is: {n1 - n2}"
+        except Exception as e:
+            return f"Error: {e}"
+
     def multiply(self):
-        n1 = int(input("Enter 1st Num: "))
-        n2 = int(input("Enter 2nd Num: "))
-        print(f"The Multiply of {n1} and {n2} is: {n1*n2}")
+        try:
+            n1 = int(input("Enter 1st Num: "))
+            n2 = int(input("Enter 2nd Num: "))
+            return f"The Multiply of {n1} and {n2} is: {n1 * n2}"
+        except Exception as e:
+            return f"Error: {e}"
+
     def division(self):
-        n1 = int(input("Enter 1st Num: "))
-        n2 = int(input("Enter 2nd Num: "))
         try:
-            print(f"The Division of {n1} and {n2} is: {n1/n2}")
+            n1 = int(input("Enter 1st Num: "))
+            n2 = int(input("Enter 2nd Num: "))
+            return f"The Division of {n1} and {n2} is: {n1 / n2}"
         except Exception as e:
-            print(f"Error: {e}")
+            return f"Error: {e}"
+
     def expression(self):
-        stringInput = input("Enter the Expression: ")
         try:
-            print(f"The Answer is: {eval(stringInput)}")
+            stringInput = input("Enter the Expression: ")
+            return f"The Answer is: {eval(stringInput)}"
         except Exception as e:
-            print(f"Expression as {e}")      
+            return f"Error in expression: {e}"
+
 
 if __name__ == "__main__":
-    print("          *****Welcome to Calculator Command Line Interface*****          ")
+    print("          ***** Welcome to Calculator Command Line Interface *****")
+
     obj = Calculator()
     switchCase = {
         1: obj.sum,
@@ -46,12 +59,17 @@ if __name__ == "__main__":
     while True:
         print("\nSelect the option below:")
         print("1. Summation (+)")
-        print("2. Substract (-)")
+        print("2. Subtract (-)")
         print("3. Multiply (*)")
         print("4. Division (/)")
         print("5. Expression (+, -, *, /)")
         print("6. Exit\n")
 
-        choice = int(input("Enter your Choice: "))
-
-        switchCase.get(choice, lambda: print("Invalid Choice, Try again..."))()
+        try:
+            choice = int(input("Enter your Choice: "))
+            results = switchCase.get(choice, lambda: print("Invalid Choice, Try again..."))()
+            
+            if (results):
+                print(f"{results}")
+        except ValueError:
+            print("Please enter a valid number!")
